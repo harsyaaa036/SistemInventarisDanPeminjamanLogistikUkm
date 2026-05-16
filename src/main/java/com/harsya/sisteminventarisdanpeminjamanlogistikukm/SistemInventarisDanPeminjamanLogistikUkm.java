6e36312d70024168a9c5b370a94f8cd8daa3e09b
harsyaaa036 <147331888+harsyaaa036@users.noreply.github.com>
2026-05-16 16:11:08 +0700

    first commit

---
+++ src/main/java/com/harsya/sisteminventarisdanpeminjamanlogistikukm/SistemInventarisDanPeminjamanLogistikUkm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.harsya.sisteminventarisdanpeminjamanlogistikukm;
+
+/**
+ *
+ * @author Hype AMD
+ */
+public class SistemInventarisDanPeminjamanLogistikUkm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
